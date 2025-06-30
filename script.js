a4af2b28ddb6da445d2d736797b20751e182d594
Hemanth Thumati <hthumath@gitam.in>
2025-06-30 16:42:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("✅ Thanks for signing up! We'll notify you when BorrowBox launches in your city.");
+});
